1b248b4a7df4b00337d7f9cfb923daeefb46e3fe
Lohit Deva <devaboi8@gmail.com>
2022-12-07 19:15:44 +0400

    Added comments

--- day-one-band-name-generator/main.py
+++ day-one-band-name-generator/main.py
@@ -1,4 +1,9 @@
+# Python script to generate a band name by combining the name of the city you grew up in with the name of your pet
+# Created by: Lohit Deva
+# 07/12/2022
+
 print("Welcome to the Band Name Generator.")
 print("Your band name could be", 
+		# Here the input functions are nested inside of the print function
 		input("What's the name of the city you grew up in?\n"), 
 		input("What's your pet's name?\n"))
