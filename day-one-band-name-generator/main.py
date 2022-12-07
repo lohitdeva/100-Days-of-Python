6f5bdf3642269d842b15fdd795bc3a88a81130ca
Lohit Deva <devaboi8@gmail.com>
2022-12-07 19:13:04 +0400

    Nested input() function inside print() function

--- day-one-band-name-generator/main.py
+++ day-one-band-name-generator/main.py
@@ -1,4 +1,4 @@
 print("Welcome to the Band Name Generator.")
-city = input("What's the name of the city you grew up in?\n")
-pet = input("What's your pet's name?\n")
-print("Your band name could be", city, pet)
+print("Your band name could be", 
+		input("What's the name of the city you grew up in?\n"), 
+		input("What's your pet's name?\n"))
