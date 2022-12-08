807da67c15d50ed86e07f45fa5c1189fbefa5d50
Lohit Deva <devaboi8@gmail.com>
2022-12-08 14:23:53 +0400

    Created file for rps [rock, paper, scissors] script

---
+++ day-4-rock-paper-scissors/main.py
@@ -0,0 +1,4 @@
+# This script will replicate the classic kids game - Rock, Paper, Scissors.
+# Fun Addition - We will use ASCII art to denote user and computer choice.
+# Created by: Lohit Deva
+# 08/12/2022
