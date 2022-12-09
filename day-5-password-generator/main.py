a09f2e8990d26d335809d274b9ae7ae7c372baf1
Lohit Deva <devaboi8@gmail.com>
2022-12-10 01:22:31 +0400

    Completed day five project - password generator

--- day-5-password-generator/main.py
+++ day-5-password-generator/main.py
@@ -5,4 +5,32 @@
 # Created by: Lohit Deva
 # 09/12/2022
 
-print("Welcome to the PyPassword Generator")
+import random
+
+print("\nWelcome to the PyPassword Generator")
+
+# Storing as string instead of list takes up considerably less space in memory
+letters = 'abcdefghijklmnopqrstuvwxyz'
+numbers = '0123456789'
+symbols = '!#$%&()*+'
+
+no_letters = int(input("\nHow many letters do you want in the password?\n"))
+no_numbers = int(input("How many numbers do you want in the password?\n"))
+no_symbols = int(input("How many symbols do you want in the password?\n"))
+
+# Here we use random.choices() to pick some items from an iterable (with replacement). The parameter
+# k has to be specified to denote how many items to pick.
+#
+# Since lists are returned, we append them directly with each other to reduce creating extra variables.
+raw_chars = random.choices(letters, k=no_letters) + random.choices(numbers, k=no_numbers) + random.choices(symbols, k=no_symbols)
+
+# random.shuffle() shuffles the list in place. It does not create a new list and thus returns None
+# Hence we use random.sample() instead
+shuffled_chars = random.sample(raw_chars, len(raw_chars))
+
+# Here, a for loop is added to concatenate all the characters in the list into a string
+password = ''
+for char in shuffled_chars:
+	password += char
+
+print(f"\nYour new password is {password}\n")	
