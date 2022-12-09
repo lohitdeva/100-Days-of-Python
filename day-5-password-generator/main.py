545f9ed0060166b1abb4e1e7908e86201f60c7dd
Lohit Deva <devaboi8@gmail.com>
2022-12-09 16:57:23 +0400

    Created day five project - Password Generator

---
+++ day-5-password-generator/main.py
@@ -0,0 +1,6 @@
+# This script will accept inputs from the user about how many letters, symbols,
+# and numbers to include and then generate a random password according to their
+# specifications
+#
+# Created by: Lohit Deva
+# 09/12/2022
