a4a91b2d31e6391d2be2b72e6a35bd07adf9c72f
Lohit Deva <devaboi8@gmail.com>
2022-12-12 01:55:08 +0400

    Refactored code to perform encryption and decrytion in a single function

--- day-8-caesar-cipher/main.py
+++ day-8-caesar-cipher/main.py
@@ -6,69 +6,40 @@
 # Created by: Lohit Deva
 # 11/12/2022
 
-# This function is to encode a given string using the Caesar cipher. The 'raw' parameter is the string that is to be encoded and
-# the 'shift' parameter is the number of characters it has to be shifted down the alphabet by.
-def encodeString(raw, shift):
+def caesar(raw, shift, direction):
+
+	# Initialized an empty string to store the result.
 	result = ''
-	
-	# This loop goes through every character in the original string and shifts it.
+
+	# Changing the shift to negative in case of decode as the shifting needs to move up the alphabet (or numbers) instead
+	# of down
+	if direction == 'd':
+		shift = -shift
+
 	for char in raw:
 
-		# The loop simply adds a blank to the resultant string and skips any computation if a blank space is encoutered.
-		if not char.isalnum() :
+		# If the character is not an alphabet or number, it can just be added to the result string as is.
+		if not char.isalnum():
 			result += char
 			continue
 
-		# This checks if the character is an alphabet and thus uses the alphabets string as reference.
-		elif char.isalpha():
-		
-			# Here, position after shifting is calculated. The modulo operator is used to ensure that position oof shifted
-			# character stays between 0 - 25 (the available indices in the alphabets string).
-			# Since we are shifting the position of the character down the alphabet, we add shift.
-			position = (alphabets.index(char.lower()) + shift) % len(alphabets)
-		
-			# This checks if the character in the original string is upper case and maintains the case.
-			if char.isupper():
-				result += alphabets[position].upper()
-		
-			# If it is not uppercase, then the character is simply appended without modification.
+		else:
+			# In case of alphabet characters, the 'alphabets' string needs to be used to shift the character.
+			if char.isalpha():
+				position = (alphabets.index(char.lower()) + shift) % len(alphabets)
+
+				# This ensures that any uppercase character remains uppercase after shifting
+				if char.isupper():
+					result += alphabets[position].upper()
+				else:
+					result += alphabets[position]
+
+			# In case of numeric characters, the 'numbers' string needs to be used to shift the characters.
 			else:
-				result += alphabets[position]
-
-		# This executes if the character is a number and uses the numbers string as reference.
-		else:		
-			
-			result += numbers[(numbers.index(char) + shift) % len(numbers)]
-
-	print(f"\nThe encoded string is: {result}\n")	
-
-# This function works similar to the encodeString() function, except that it moves the characters up the alphabet by the 
-# specified number of shifts and thus can decode a cipher created by the encodeString() function if the same shift is used.
-def decodeString(raw, shift):
-	result = ''
+				result += numbers[(numbers.index(char) + shift) % len(numbers)]
 	
-	for char in raw:
-
-		if not char.isalnum() :
-			result += char
-			continue
-
-		# Here we subtract shift since we are moving up the alphabet. 
-		elif char.isalpha():
-		
-			position = (alphabets.index(char.lower()) - shift) % len(alphabets)
-		
-			if char.isupper():
-				result += alphabets[position].upper()
-		
-			else:
-				result += alphabets[position]
-
-		else:		
-			
-			result += numbers[(numbers.index(char) - shift) % len(numbers)]
-
-	print(f"\nThe encoded string is: {result}\n")	
+	# Here we use a ternary operation to display the appropriate text based on the conversion that was carried out.
+	print(f'Your {"encoded" if direction == "e" else "decoded"} text is: {result}')
 
 # This string contains the letters of the alphabet to use as a reference when shifting characters while encoding/decoding.
 alphabets = 'abcdefghijklmnopqrstuvwxyz'
@@ -101,10 +72,10 @@ while loop_continue == 'y':
 		shift = int(input("Enter the number of characters to be shifted by:\n"))
 
 		if choice == 'encode':
-			encodeString(raw_string, shift)
+			caesar(raw_string, shift, 'e')
 
 		else:
-			decodeString(raw_string, shift)
+			caesar(raw_string, shift, 'd')
 
 	# Here the condition for the while loop is updated. If the user enters 'y' (or 'Y'), the initial condition stays true
 	# and the loop continues for another iteration. However if any different input is entered, the loop is immediately terminated.
