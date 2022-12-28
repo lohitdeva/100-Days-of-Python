c1e359cf29d4001324763af7851c7a79128dc9d0
Lohit Deva <devaboi8@gmail.com>
2022-12-28 18:50:10 +0000

    Testing changes made from GCP VM

--- day-14-higher-lower/main.py
+++ day-14-higher-lower/main.py
@@ -5,3 +5,6 @@
 # 4. If the user guesses correctly, assign the value of 'b' to 'a' and select a new celebrity at random for 'b'.
 # 5. Continue until user gets an incorrect guess.
 # 6. If user guesses incorrectly, clear console, display game over message and end game.
+import random
+
+
