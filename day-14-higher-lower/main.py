fb788f8567fc56dc62ff68be95b9ed1f5308ffcc
Lohit Deva <devaboi8@gmail.com>
2022-12-28 18:29:01 +0400

    Created the required files for day 14 project

---
+++ day-14-higher-lower/main.py
@@ -0,0 +1,7 @@
+#TODO:
+# 1. Pick 2 celebrities randomly from game_data.py ('a' and 'b')
+# 2. Display the celebrity info in the required format.
+# 3. Ask user to guess which of the two has a higher follower count
+# 4. If the user guesses correctly, assign the value of 'b' to 'a' and select a new celebrity at random for 'b'.
+# 5. Continue until user gets an incorrect guess.
+# 6. If user guesses incorrectly, clear console, display game over message and end game.
