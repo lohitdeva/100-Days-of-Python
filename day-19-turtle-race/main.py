bac506987bbd9e98b1072fdbbb95e826333a46c7
Lohit Deva <devaboi8@gmail.com>
2022-12-31 16:20:01 +0400

    Cleaned up code to fix error when no input is given. Also added comments

--- day-19-turtle-race/main.py
+++ day-19-turtle-race/main.py
@@ -4,24 +4,34 @@ import random
 s = Screen()
 
 s.setup(width=1000, height=800)
-s.bgpic('day-19/background.gif')
+# Background images must be saved as gif, other formats do not work
+s.bgpic('day-19-turtle-race/background.gif')
 s.title('Turtle GP 2022')
 
 user_bet = s.textinput(
     title='Make your bet',
     prompt=
     'Which turtle will win the race? Enter a color:\n\n(red/orange/yellow/green/blue/purple)'
-).strip().lower()
+)
+
+# Exits the script and closes the turtle window if user clicks on cancel
+if not user_bet:
+    s.bye()
+    quit()
+user_bet = user_bet.strip().split()
+
 colors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple']
 
+# Prompts user to type in the correct color if they enter an invalid one
 while user_bet not in colors:
     user_bet = s.textinput(
         title='Wrong choice',
         prompt='That color does not exist, please try again:')
 
 turtle_list = list()
-is_race_on = False
+is_race_on = True
 
+# Creates each turtle object, sets their colors and starting positions and adds them to a list
 for i in range(1, 7):
     turtle = Turtle(shape="turtle")
     turtle.resizemode('user')
@@ -33,9 +43,7 @@ for i in range(1, 7):
     turtle.speed('fastest')
     turtle_list.append(turtle)
 
-if user_bet and user_bet in colors:
-    is_race_on = True
-
+# Keep moving the turtles randomly until they reach the finish line
 while is_race_on:
     for turtle in turtle_list:
         turtle.forward(random.randint(1, 10))
@@ -44,6 +52,7 @@ while is_race_on:
             winner = turtle.pencolor()
             break
 
+# Display the winning turtle's color in the console
 print(f"The winner is '{winner}'")
 if winner == user_bet:
     print("You win! 💲💲💲")
